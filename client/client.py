a024b68e1bb0b46be0800818f9533a00df90ad01
paulo.barros <paulo.barros@esfera5.com.br>
2019-07-14 17:13:36 +0000

    wip

--- client/client.py
+++ client/client.py
@@ -4,7 +4,7 @@ import pyne_pb2
 import pyne_pb2_grpc
 
 def SayHello(text):
-    with grpc.insecure_channel('greeter:3000') as channel:
+    with grpc.insecure_channel('pyne:3000') as channel:
         stub = pyne_pb2_grpc.GreeterStub(channel)
         req = pyne_pb2.HelloRequest(name=text)
         response = stub.SayHello(req)
